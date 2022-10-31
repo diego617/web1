b6313243b364ced60acef5c40d0960e2b9ed4777
diego617 <diegotacuricabezas617@gmail.com>
2022-10-30 23:31:04 -0500

    Agrege un array para el menu y puse estilos al ul li a

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+const ul = document.getElementById('ul');
 const menu = ['Incio','Productos','Blog','Contacto'];
 
-for (const i in menu){
-    const e = menu[i];
-    console.log(e)
+
+for(const i in menu){
+    const e = menu[i]
+    ul.innerHTML += `<li><a>${e}</a></li>`;
 }
+
