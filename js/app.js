529a5fc2f608c15bdeeb4b1869240914b86f2618
diego617 <diegotacuricabezas617@gmail.com>
2022-11-02 23:19:28 -0500

    Agrege el evento mouseover

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
-const ul = document.getElementById('ul');
-const menu = ['Incio','Productos','Blog','Contacto'];
+const ul = document.querySelector('ul');
+const a = document.querySelectorAll('a');
+const color = true;
+/*a.addEventListener('mouseover',()=>{
+    //console.log(e.target);
+    a.style.color= 'red';
+});*/
 
 
-for(const i in menu){
-    const e = menu[i]
-    ul.innerHTML += `<li><a>${e}</a></li>`;
+for(let i = 0; i < a.length; i++){
+    a[i].addEventListener('mouseover',(e)=>{
+        //console.log(e.target.parentElement.parentElement);
+        let aColor = e.target;
+        if(color == true ){
+            aColor.style.color = "green";
+        }else{aColor.style.color = "";}
+        
+          
+    });
 }
+
 
